7a95afbe78fc92956280e0ae86bb51116ee98576
Osier Nono <osiernono@gmail.com>
2025-08-28 13:36:43 -0500

    testing and finishing the code

--- bsort.cpp
+++ bsort.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <list>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,17 +11,47 @@ int main()
 {
 
     string bname;
-    string yon;
-    cout<< "Welcome to the Business Sorting Program"<< endl;
-    cout<< "Please enter the name of the business"<< endl;
+    string yesono;
+    bool yon= true;
+    cout<< "Welcome to the Business Sorting Program!!"<< endl;
+    cout<< "Please enter the name of the business:"<< endl;
     cin>> bname;
 
-    list<string> busList= {};
-
-    for(int i=0; i< busList.size(); i++)
+    vector<string> busList;
+    busList.push_back(bname);
+    cout<< "Your businesses are:"<< endl;
+    for(string i: busList)
     {
-
+        cout<< i<< "\n";
     }
 
+    while(yon)
+    {
+        cout<< "Another business? "<< endl;
+        cin>> yesono;
+
+        if(yesono=="yes"|| yesono=="Yes"|| yesono=="y"|| yesono=="Y")
+        {
+            yon=true;
+            cout<< "Please enter the name of the business:"<< endl;
+            cin>>bname;
+
+            busList.push_back(bname);
+            sort(busList.begin(),busList.end()); 
+            
+            cout<<"Your businesses are:"<<endl;
+
+            for(string i: busList)
+            {
+                cout<< i<< "\n";
+            }
+        }
+        else
+        {
+            yon=false;
+            cout<< "thank you for using the Business Sorting Program!"<< endl;
+        }
+    }
+        return 0;
 
 }
