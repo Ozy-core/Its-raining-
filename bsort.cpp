3edcc5198dd207e53fb61443f0eb9330efe04757
Osier Nono <osiernono@gmail.com>
2025-08-28 12:26:01 -0500

    staring itsrainign code

---
+++ bsort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+
+    string bname;
+    string yon;
+    cout<< "Welcome to the Business Sorting Program"<< endl;
+    cout<< "Please enter the name of the business"<< endl;
+    cin>> bname;
+
+    list<string> busList= {};
+
+    for(int i=0; i< busList.size(); i++)
+    {
+
+    }
+
+
+}
